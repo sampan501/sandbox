934538891a2d2edad4908ad36cb742f54c133f94
Sambit Panda <spanda3@jhu.edu>
2020-04-01 08:07:02 -0400

    add corona animation

--- R/hotelling-manova-unittest.R
+++ R/hotelling-manova-unittest.R
@@ -1,13 +1,18 @@
 require(mgc)
 require(Hotelling)
 
-deg2rad <- function(deg) {(deg * pi) / (180)}
+# data <- mgc.sims.linear(1000, 1, eps = 0)
+# data <- cbind(data$X, data$Y)
+# 
+# rot_mat <- matrix(data = c(0, 1, -1, 0), nrow=2, ncol=2)
+# new_data <- t(rot_mat %*% t(data))
+# stat <- hotelling.test(data, new_data)
+# print(paste("Statistic: ", stat$stats$statistic))
+# print(paste("P-value: ", stat$pval))
 
-data <- mgc.sims.linear(1000, 1, eps = 0)
-data <- cbind(data$X, data$Y)
 
-angle <- deg2rad(90)
-rot_mat <- matrix(data = c(cos(angle), sin(angle), -sin(angle), cos(angle)), nrow=2, ncol=2)
+data <- matrix(seq(1000) - 1, nrow=500, ncol=2)
+rot_mat <- matrix(data = c(0, 1, -1, 0), nrow=2, ncol=2)
 new_data <- t(rot_mat %*% t(data))
 stat <- hotelling.test(data, new_data)
 print(paste("Statistic: ", stat$stats$statistic))
